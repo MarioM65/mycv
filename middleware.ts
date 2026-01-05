d662ec4bfa60a11593a50122ab8178f32055e6be
MarioM65 <mmystirio7@gmail.com>
2026-01-05 20:40:56 +0100

    correção do nome middleware

---
+++ middleware.ts
@@ -0,0 +1,2 @@
+//midleware de teste, apenas para ver se o midleware funciona
+console.log('Middleware is running');
--- midleware.ts
+++
@@ -1,5 +0,0 @@
-//midleware de teste, apenas para ver se o midleware funciona
-export function middleware(request: Request) {
-    console.log('Middleware is working!');
-    return new Response('Middleware response', { status: 200 });
-}
