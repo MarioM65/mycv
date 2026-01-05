55e4deadb353db9cdb97f6a0bd403bb5aac58c3f
MarioM65 <mmystirio7@gmail.com>
2026-01-05 20:49:21 +0100

    correção do middleware

--- middleware.ts
+++ middleware.ts
@@ -1,2 +1,13 @@
-//midleware de teste, apenas para ver se o midleware funciona
-console.log('Middleware is running');
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+ 
+export function middleware(request: NextRequest) {
+    console.log('Middleware executed for:', request.nextUrl.pathname)
+  if (request.nextUrl.pathname.startsWith('/teste')) {
+    return NextResponse.rewrite(new URL('/teste-2', request.url))
+  }
+ 
+  if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.rewrite(new URL('/dashboard/user', request.url))
+  }
+}
